2e7fa17c65e65a11c917382c9e57842d79059d0a
yuzhehan <yuzhehan@ft.tech>
2024-08-09 19:09:58 +0800

    初始化

---
+++ http/src/httpresponse.rs
@@ -0,0 +1,57 @@
+use std::collections::HashMap;
+use std::io::{Result, Write};
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct HttpResponse<'a> {
+    version: &'a str,
+    status_code: &'a str,
+    status_text: &'a str,
+    headers: Option<HashMap<&'a str, &'a str>>,
+    body: Option<String>,
+}
+
+impl<'a> Default for HttpResponse<'a> {
+    fn default() -> Self {
+        Self {
+            version: "HTTP/1.1".into(),
+            status_code: "200".into(),
+            status_text: "OK".into(),
+            headers: None,
+            body: None
+        }
+    }
+}
+
+impl<'a> HttpResponse<'a> {
+    pub fn new(
+        status_code: &'a str,
+        headers: Option<HashMap<&'a str, &'a str>>,
+        body: Option<String>,
+    ) -> HttpResponse<'a> {
+        let mut response: HttpResponse<'a> = HttpResponse::default();
+
+        if status_code != "200" {
+            response.status_code = status_code.into();
+        }
+
+        response.headers = match &headers {
+            Some(_h) => headers,
+            None => {
+                let mut h = HashMap::new();
+                h.insert("Content-Type", "text/html");
+                Some(h)
+            }
+        };
+
+        response.status_text = match response.status_code {
+            "200" => "OK".into(),
+            "400" => "Bad Request".into(),
+            "404" => "Not Found".into(),
+            "500" => "Internal Server Error".into(),
+            _ => "Not Found".into(),
+        };
+
+        response.body = body;
+        response
+    }
+}
