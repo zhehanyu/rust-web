52138869a04730add4386193b2de2e67f7029922
zhehanyu <zhehanyu@gmail.com>
2024-08-10 15:29:47 +0800

    完成相应和请求模块

--- http/src/httpresponse.rs
+++ http/src/httpresponse.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::io::{Result, Write};
 
-#[derive(Debug, PartialEq, Clone)]
+# [derive(Debug, PartialEq, Clone)]
 pub struct HttpResponse<'a> {
     version: &'a str,
     status_code: &'a str,
@@ -22,13 +22,28 @@ impl<'a> Default for HttpResponse<'a> {
     }
 }
 
+impl<'a> From<HttpResponse<'a>> for String {
+    fn from(value: HttpResponse<'a>) -> Self {
+        let res = value.clone();
+        format!(
+            "{} {} {}\r\n{}Content-Length: {}\r\n\r\n{}",
+            &res.version(),
+            &res.status_code(),
+            &res.status_text(),
+            &res.headers(),
+            &value.body.unwrap().len(),
+            &res.body()
+        )
+    }
+}
+
 impl<'a> HttpResponse<'a> {
     pub fn new(
         status_code: &'a str,
         headers: Option<HashMap<&'a str, &'a str>>,
         body: Option<String>,
     ) -> HttpResponse<'a> {
-        let mut response: HttpResponse<'a> = HttpResponse::default();
+        let mut response = HttpResponse::default();
 
         if status_code != "200" {
             response.status_code = status_code.into();
@@ -53,5 +68,112 @@ impl<'a> HttpResponse<'a> {
 
         response.body = body;
         response
+    }
+
+    pub fn send_respones(&self, write_stream: &mut impl Write) -> Result<()> {
+        let res = self.clone();
+        let response_string = String::from(res);
+        let _ = write!(write_stream, "{}", response_string);
+
+
+        Ok(())
+    }
+
+    fn version(&self) -> &str {
+        self.version
+    }
+
+    fn status_code(&self) -> &str {
+        self.status_code
+    }
+
+    fn status_text(&self) -> &str {
+        self.status_text
+    }
+
+    fn headers(&self) -> String {
+        let map = self.headers.clone().unwrap();
+        let mut header_string = "".into();
+        for (k, v) in map.iter() {
+            header_string = format!("{}{}:{}\r\n", header_string, k, v);
+        }
+        header_string
+    }
+
+    pub fn body(&self) -> &str {
+        match &self.body {
+            Some(b) => b.as_str(),
+            None => "",
+        }
+    }
+}
+
+
+# [cfg(test)]
+mod tests {
+    use super::*;
+
+    # [test]
+    fn test_response_struct_creation_200() {
+        let response_actual = HttpResponse::new(
+            "200",
+            None,
+            Some("xxxx".into()),
+        );
+
+        let response_expected = HttpResponse {
+            version: "HTTP/1.1",
+            status_code: "200" ,
+            status_text: "OK",
+            headers: {
+                let mut h = HashMap::new();
+                h.insert("Content-Type", "text/html");
+                Some(h)
+            },
+            body: Some("xxxx".into()),
+        };
+        assert_eq!(response_actual, response_expected);
+    }
+
+    # [test]
+    fn test_response_struct_creation_404() {
+        let response_actual = HttpResponse::new(
+            "404",
+            None,
+            Some("xxxx".into()),
+        );
+
+        let response_expected = HttpResponse {
+            version: "HTTP/1.1",
+            status_code: "404" ,
+            status_text: "Not Found",
+            headers: {
+                let mut h = HashMap::new();
+                h.insert("Content-Type", "text/html");
+                Some(h)
+            },
+            body: Some("xxxx".into()),
+        };
+        assert_eq!(response_actual, response_expected);
+    }
+
+    # [test]
+    fn test_http_response_creation() {
+        let response_expected = HttpResponse {
+            version: "HTTP/1.1",
+            status_code: "404" ,
+            status_text: "Not Found",
+            headers: {
+                let mut h = HashMap::new();
+                h.insert("Content-Type", "text/html");
+                Some(h)
+            },
+            body: Some("xxxx".into()),
+        };
+
+        let http_string: String = response_expected.into();
+
+        let actual_string = "HTTP/1.1 404 Not Found\r\nContent-Type:text/html\r\nContent-Length: 4\r\n\r\nxxxx";
+        assert_eq!(http_string, actual_string);
     }
 }
