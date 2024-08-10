c339c205e24cdf292ce1d0ee103db7a590274096
zhehanyu <zhehanyu@gmail.com>
2024-08-10 17:04:41 +0800

    完成router handler

---
+++ httpserver/src/router.rs
@@ -0,0 +1,32 @@
+use super::handler::{Handler, PageNotFoundHandler, StaticPageHandler, WebServiceHandler};
+use http::{httprequest, httprequest::HttpRequest, httpresponse, httpresponse::HttpResponse};
+
+use std::io::prelude::*;
+
+pub struct Router;
+
+impl Router {
+    pub fn route(req: HttpRequest, stream: &mut impl Write) -> () {
+        match req.method {
+            httprequest::Method::Get => match &req.resource {
+                httprequest::Resourse::Path(s) => {
+                    let route: Vec<&str> = s.split("/").collect();
+                    match route[1] {
+                        "api" => {
+                            let resp: HttpResponse = WebServiceHandler::handle(&req);
+                            let _ = resp.send_respones(stream);
+                        }
+                        _ => {
+                            let resp: HttpResponse = StaticPageHandler::handle(&req);
+                            let _ = resp.send_respones(stream);
+                        }
+                    }
+                }
+            },
+            _ => {
+                let resp: HttpResponse = PageNotFoundHandler::handle(&req);
+                let _ = resp.send_respones(stream);
+            }
+        }
+    }
+}
