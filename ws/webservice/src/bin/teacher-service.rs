830b6eab672738a41f3140ce5444962b6e645d9d
zhehanyu <zhehanyu@gmail.com>
2024-08-11 16:03:09 +0800

    完善webservice

--- ws/webservice/src/bin/teacher-service.rs
+++ ws/webservice/src/bin/teacher-service.rs
@@ -6,6 +6,8 @@ use std::sync::Mutex;
 mod handlers;
 # [path = "../state.rs"]
 mod state;
+# [path = "../models.rs"]
+mod models;
 # [path = "../routers.rs"]
 mod routers;
 
@@ -17,12 +19,14 @@ async fn main() -> io::Result<()>{
     let shared_data = web::Data::new(AppState {
         health_check_response: "I'm OK.".to_string(),
         visit_count: Mutex::new(0),
+        courses: Mutex::new(vec![]),
     });
 
     let app = move || {
         App::new()
             .app_data(shared_data.clone())
             .configure(general_routes)
+            .configure(course_routes)
     };
 
     HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
